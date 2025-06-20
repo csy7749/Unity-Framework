34a17734d6924e5d05871f9cb9737ccaeb6ba4e1
Neverland <578463782@qq.com>
2025-06-21 01:05:13 +0800

    update obfuz

--- Project.Framework/Assets/UnityFramework/Editor/Obfuz/Bootstrap.cs
+++ Project.Framework/Assets/UnityFramework/Editor/Obfuz/Bootstrap.cs
@@ -1,31 +1,31 @@
-using HybridCLR.Editor;
-using Obfuz4HybridCLR;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using UnityEditor;
-using UnityEngine;
-
-namespace UnityFramework.Editor
-{
-    public static class BuildCommand
-    {
-        [MenuItem("Build/CompileAndObfuscateAndCopyToStreamingAssets")]
-        public static void CompileAndObfuscateAndCopyToStreamingAssets()
-        {
-            BuildTarget target = EditorUserBuildSettings.activeBuildTarget;
-            ObfuscateUtil.ObfuscateHotUpdateAssemblies(target,"test");
-
-            Directory.CreateDirectory(Application.streamingAssetsPath);
-
-            string hotUpdateDllPath = $"{SettingsUtil.GetHotUpdateDllsOutputDirByTarget(target)}";
-            foreach (string assName in SettingsUtil.HotUpdateAssemblyNamesIncludePreserved)
-            {
-                string srcFile = $"{hotUpdateDllPath}/{assName}.dll";
-                string dstFile = $"{Application.streamingAssetsPath}/{assName}.dll.bytes";
-                File.Copy(srcFile, dstFile, true);
-                Debug.Log($"[CompileAndObfuscate] Copy {srcFile} to {dstFile}");
-            }
-        }
-    }   
-}
+// using HybridCLR.Editor;
+// using Obfuz4HybridCLR;
+// using System.Collections;
+// using System.Collections.Generic;
+// using System.IO;
+// using UnityEditor;
+// using UnityEngine;
+//
+// namespace UnityFramework.Editor
+// {
+//     public static class BuildCommand
+//     {
+//         [MenuItem("Build/CompileAndObfuscateAndCopyToStreamingAssets")]
+//         public static void CompileAndObfuscateAndCopyToStreamingAssets()
+//         {
+//             BuildTarget target = EditorUserBuildSettings.activeBuildTarget;
+//             ObfuscateUtil.ObfuscateHotUpdateAssemblies(target,"test");
+//
+//             Directory.CreateDirectory(Application.streamingAssetsPath);
+//
+//             string hotUpdateDllPath = $"{SettingsUtil.GetHotUpdateDllsOutputDirByTarget(target)}";
+//             foreach (string assName in SettingsUtil.HotUpdateAssemblyNamesIncludePreserved)
+//             {
+//                 string srcFile = $"{hotUpdateDllPath}/{assName}.dll";
+//                 string dstFile = $"{Application.streamingAssetsPath}/{assName}.dll.bytes";
+//                 File.Copy(srcFile, dstFile, true);
+//                 Debug.Log($"[CompileAndObfuscate] Copy {srcFile} to {dstFile}");
+//             }
+//         }
+//     }   
+// }
