72ba1b8d5b102bc65530a7d923b6e17c5a607656
Neverland <578463782@qq.com>
2025-07-17 19:15:57 +0800

    clear egameplay setting

--- Project.Framework/Assets/Editor/UnityFrameworkSettingsProvider/UnityFrameworkEGamePlaySettingsProvider.cs
+++ Project.Framework/Assets/Editor/UnityFrameworkSettingsProvider/UnityFrameworkEGamePlaySettingsProvider.cs
@@ -3,42 +3,42 @@ using UnityEditor;
 
 public static class UnityFrameworkEGamePlaySettingsProvider
 {
-    [MenuItem("UnityFramework/Settings/UnityFramework EGamePlaySetting", priority = -1)]
-    public static void OpenSettings() => SettingsService.OpenProjectSettings("Project/UnityFramework/EGamePlaySetting");
+    [MenuItem("UnityFramework/Settings/UnityFramework EGamePlaySettings", priority = -1)]
+    public static void OpenSettings() => SettingsService.OpenProjectSettings("Project/UnityFramework/EGamePlaySettings");
     
-    private const string SettingsPath = "Project/UnityFramework/EGamePlaySetting";  
+    private const string SettingsPath = "Project/UnityFramework/EGamePlaySettings";  
     
     [SettingsProvider]  
     public static SettingsProvider CreateMySettingsProvider()  
     {  
         return new SettingsProvider(SettingsPath, SettingsScope.Project)  
         {  
-            label = "UnityFramework/UpdateSettings",  
-            guiHandler = (searchContext) =>  
-            {  
-                var settings = Settings.UpdateSetting;  
-                var serializedObject = new SerializedObject(settings);  
-
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("projectName"));  
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("HotUpdateAssemblies"));  
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("AOTMetaAssemblies"));  
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("LogicMainDllName"));  
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("AssemblyTextAssetExtension"));  
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("AssemblyTextAssetPath"));  
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("UpdateStyle"));  
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("EnableUpdateData"));
-                if (settings.EnableUpdateData)
-                {
-                    EditorGUILayout.PropertyField(serializedObject.FindProperty("ServerStateDatePath"));  
-                    EditorGUILayout.PropertyField(serializedObject.FindProperty("ServerStateDataFileName"));  
-                    EditorGUILayout.PropertyField(serializedObject.FindProperty("UpdateDataPath"));  
-                    EditorGUILayout.PropertyField(serializedObject.FindProperty("UpdateDataFileName"));  
-                }
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("ResDownLoadPath"));  
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("FallbackResDownLoadPath"));  
-                serializedObject.ApplyModifiedProperties();  
-            },  
-            keywords = new[] { "UnityFramework", "Settings", "Custom" }  
+            label = "UnityFramework/EGamePlaySettings",  
+            // guiHandler = (searchContext) =>  
+            // {  
+            //     var settings = Settings.UpdateSetting;  
+            //     var serializedObject = new SerializedObject(settings);  
+            //
+            //     EditorGUILayout.PropertyField(serializedObject.FindProperty("projectName"));  
+            //     EditorGUILayout.PropertyField(serializedObject.FindProperty("HotUpdateAssemblies"));  
+            //     EditorGUILayout.PropertyField(serializedObject.FindProperty("AOTMetaAssemblies"));  
+            //     EditorGUILayout.PropertyField(serializedObject.FindProperty("LogicMainDllName"));  
+            //     EditorGUILayout.PropertyField(serializedObject.FindProperty("AssemblyTextAssetExtension"));  
+            //     EditorGUILayout.PropertyField(serializedObject.FindProperty("AssemblyTextAssetPath"));  
+            //     EditorGUILayout.PropertyField(serializedObject.FindProperty("UpdateStyle"));  
+            //     EditorGUILayout.PropertyField(serializedObject.FindProperty("EnableUpdateData"));
+            //     if (settings.EnableUpdateData)
+            //     {
+            //         EditorGUILayout.PropertyField(serializedObject.FindProperty("ServerStateDatePath"));  
+            //         EditorGUILayout.PropertyField(serializedObject.FindProperty("ServerStateDataFileName"));  
+            //         EditorGUILayout.PropertyField(serializedObject.FindProperty("UpdateDataPath"));  
+            //         EditorGUILayout.PropertyField(serializedObject.FindProperty("UpdateDataFileName"));  
+            //     }
+            //     EditorGUILayout.PropertyField(serializedObject.FindProperty("ResDownLoadPath"));  
+            //     EditorGUILayout.PropertyField(serializedObject.FindProperty("FallbackResDownLoadPath"));  
+            //     serializedObject.ApplyModifiedProperties();  
+            // },  
+            // keywords = new[] { "UnityFramework", "Settings", "Custom" }  
         };  
     }
 }
--- Project.Framework/Assets/GameScripts/HotFix/GameLogic/Demo/HomeSystem.cs
+++ Project.Framework/Assets/GameScripts/HotFix/GameLogic/Demo/HomeSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Cysharp.Threading.Tasks;
+using UnityFramework;
 
 namespace GameLogic
 {
